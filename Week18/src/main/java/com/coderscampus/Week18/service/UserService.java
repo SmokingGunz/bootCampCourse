320e6f5faf6f9989e02375a7e169924892a276d1
SmokingGunz <ericowens08@gmail.com>
2023-07-12 21:21:35 -0700

    small updates to Week18

--- Week18/src/main/java/com/coderscampus/Week18/service/UserService.java
+++ Week18/src/main/java/com/coderscampus/Week18/service/UserService.java
@@ -1,40 +1,45 @@
 package com.coderscampus.Week18.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.coderscampus.Week18.domain.User;
+import com.coderscampus.Week18.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.coderscampus.Week18.domain.User;
-import com.coderscampus.Week18.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-	@Autowired
-	private UserRepository userRepo;
+    @Autowired
+    private UserRepository userRepo;
 
-	public List<User> findAll() {
+    public List<User> findAll() {
 
-		return userRepo.findAll();
-	}
+        return userRepo.findAll();
+    }
 
-	public User findById(Long userId) {
+    public User findById(Long userId) {
 
-		Optional<User> userOpt = userRepo.findById(userId);
+        Optional<User> userOpt = userRepo.findById(userId);
 
-		return userOpt.orElse(new User());
-	}
+        return userOpt.orElse(new User());
+    }
 
-	public User saveUser(User user) {
+    public User saveUser(User user) {
 
-		return userRepo.save(user);
-	}
+        return userRepo.save(user);
+    }
 
-	public void delete(Long userId) {
+    public void delete(Long userId) {
 
-		userRepo.deleteById(userId);
-	}
+        userRepo.deleteById(userId);
+    }
+
+    // method to find user by username
+//    public User findByUsername(String username) {
+//
+//        return userRepo.findByUsername(username);
+//    }
 
 }
