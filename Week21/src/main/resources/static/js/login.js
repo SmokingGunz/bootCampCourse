aad8a33c8c143e002043e885a7238c4b9e0af154
SmokingGunz <ericowens08@gmail.com>
2023-08-15 22:33:38 -0700

    worked through week21.9

---
+++ Week21/src/main/resources/static/js/login.js
@@ -0,0 +1,39 @@
+var submitBtn = document.querySelector('#submitBtn') // this selects the button by id and assigns it to a variable
+// console.log(`submitBtn is : `)
+// console.log(submitBtn)
+var users = []
+
+submitBtn.addEventListener('click', (event) => {
+    var username = document.querySelector('#username')
+    var password = document.querySelector('#password')
+
+    if (username.value == '' || password.value == '') {
+        alert('Please enter your username and password')
+    } else {
+        console.log('Inputs look valid, proceed with submission')
+        var user = {
+            username: username.value,
+            password: password.value
+
+        }
+
+        users.push(user)
+    }
+})
+
+// submitBtn.addEventListener('mouseover', function () {
+//     alert(`Hey! I've been moused over!`)
+
+// })
+
+// var usernameTextBox = document.querySelector('#username') // this selects the username
+
+// // different way to add an event listener function with lambda function
+// usernameTextBox.addEventListener('focus', () => console.log(`The username Text Box now has focus!`)
+// )
+
+// // public void myFunction() {}  -- this is java code for comparison purposes
+// function submitBtnClick() {
+//     alert("Hey! I've been clicked")
+
+// }
