a857856a17c66bbf537008765c90045895d478be
SmokingGunz <ericowens08@gmail.com>
2023-08-18 23:22:19 -0700

    worked through week22.5

--- Week21/src/main/resources/static/js/register.js
+++ Week21/src/main/resources/static/js/register.js
@@ -11,12 +11,38 @@ usernameTextBox.addEventListener('blur', () => {
         },
         body: JSON.stringify(user)
     })
-    .then((response) => response.json())
-    .then((data) => {
-        if (data === true) {
-            console.log('username already exists')
-            usernameTextBox.focus()
-            usernameTextBox.select()
-        } else { }
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data === true) {
+                console.log('username already exists')
+                usernameTextBox.focus()
+                usernameTextBox.select()
+                showErrorAnimation(() => {
+                    // animation is completed at this point
+                    console.log("we are now in the callback function")
+
+                    usernameTextBox.style.backgroundColor = `rgb(255, 255, 255)`;
+                })
+            }
+        })
 });
+
+function showErrorAnimation(callback) {
+    console.log("We're in the showErrorAnimation function");
+
+    var i = 255;
+
+    var animationInterval = setInterval(() => {
+        i--;
+
+        // Decrease the green and blue components from 255 to 0
+        usernameTextBox.style.backgroundColor = `rgb(255, ${i}, ${i})`;
+
+        if (i === 0) {
+            clearInterval(animationInterval);
+            callback();
+        }
+    }, 1);
+
+    console.log("Done executing the showErrorAnimation function");
+}
