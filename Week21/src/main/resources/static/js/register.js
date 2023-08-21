f36d2b78ddade2f7a44eb2e9f09e532a20512ba8
SmokingGunz <ericowens08@gmail.com>
2023-08-21 15:17:07 -0700

    worked through week22.7

--- Week21/src/main/resources/static/js/register.js
+++ Week21/src/main/resources/static/js/register.js
@@ -1,26 +1,27 @@
 var usernameTextBox = document.querySelector(`#username`);
+var eyeIcons = document.querySelectorAll(`.fa-eye`);
 
-// this is for demonstration purposes only
-function myPromise() {
+eyeIcons.forEach((eyeIcon) => {
 
-    return new Promise((resolve, reject) => {
-        let i = 2;
+    eyeIcon.addEventListener('click', () => {
 
-        if (i === 1) {
-            resolve("hey, i === 1, so we're cool");
+        if (eyeIcon.classList.contains('fa-eye')) {
+            eyeIcon.classList.replace('fa-eye', 'fa-eye-slash');
+            if  (eyeIcon.getAttribute('id') === 'passwordEyeIcon') {
+                document.querySelector(`#password`).type = 'text';
+            } else {
+                document.querySelector(`#confirmPassword`).type = 'text';
+            };
         } else {
-            reject("Absolute fail: i is not 1, boooo");
-        }
-
-    })
-}
-
-myPromise().then((data) => {
-    console.log(data);
-}).catch((error) => {
-    console.log("Absolute fail: i is not 1, boooo");
+            eyeIcon.classList.replace('fa-eye-slash', 'fa-eye');
+            if  (eyeIcon.getAttribute('id') === 'passwordEyeIcon') {
+                document.querySelector(`#password`).type = 'password';
+            } else {
+                document.querySelector(`#confirmPassword`).type = 'password';
+            };
+        };
+    });
 });
-
 
 // HOW TO USE PROMISES IN JS
 
@@ -35,47 +36,67 @@ usernameTextBox.addEventListener('blur', () => {
         },
         body: JSON.stringify(user)
     })
-        .then((response) => response.json())
-        .then((data) => {
-            if (data === true) {
-                console.log('username already exists');
-                usernameTextBox.focus();
-                usernameTextBox.select();
-                return showErrorAnimation();
-            }
-        })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data === true) {
+            console.log('username already exists');
+            usernameTextBox.focus();
+            usernameTextBox.select();
+            return showErrorAnimation();
+        }
+    })
         .then((message) => {
             console.log("we are now in the Promise's then method after the animation");
             console.log(message);
             usernameTextBox.style.backgroundColor = `rgb(255, 255, 255)`;
         });
-});
-
-
-function showErrorAnimation() {
-    return new Promise((resolve) => {
-        console.log("We're in the showErrorAnimation function");
-
-        var i = 255;
-
-        var animationInterval = setInterval(() => {
-            i--;
-
-            // Decrease the green and blue components from 255 to 0
-            usernameTextBox.style.backgroundColor = `rgb(255, ${i}, ${i})`;
-
-            if (i === 0) {
-                clearInterval(animationInterval);
-                // console.log("Done executing the showErrorAnimation function");
-                resolve("Done executing the showErrorAnimation function");
-            }
-        }, 1);
     });
-}
-
-
-// HOW TO USE CALLBACK FUNCTION IN JS
-
+    
+    
+    function showErrorAnimation() {
+        return new Promise((resolve) => {
+            console.log("We're in the showErrorAnimation function");
+            
+            var i = 255;
+            
+            var animationInterval = setInterval(() => {
+                i--;
+                
+                // Decrease the green and blue components from 255 to 0
+                usernameTextBox.style.backgroundColor = `rgb(255, ${i}, ${i})`;
+                
+                if (i === 0) {
+                    clearInterval(animationInterval);
+                    // console.log("Done executing the showErrorAnimation function");
+                    resolve("Done executing the showErrorAnimation function");
+                }
+            }, 1);
+        });
+    }
+    
+    // this is for demonstration purposes only
+    // function myPromise() {
+    
+    //     return new Promise((resolve, reject) => {
+    //         let i = 2;
+    
+    //         if (i === 1) {
+    //             resolve("hey, i === 1, so we're cool");
+    //         } else {
+    //             reject("Absolute fail: i is not 1, boooo");
+    //         }
+    
+    //     })
+    // }
+    
+    // myPromise().then((data) => {
+    //     console.log(data);
+    // }).catch((error) => {
+    //     console.log("Absolute fail: i is not 1, boooo");
+    // });
+    
+    // HOW TO USE CALLBACK FUNCTION IN JS
+    
 // usernameTextBox.addEventListener('blur', () => {
 //     var user = {
 //         'username': usernameTextBox.value,
